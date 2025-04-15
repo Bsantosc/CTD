072e81fa4dc35df541df6f886c4407c037d24ce9
Brayan Santos <brayansantos@Mac.lan>
2025-04-14 23:52:10 -0400

    Implement student_email_list on Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,5 +3,17 @@ class Course < ApplicationRecord
   belongs_to :trimester
   has_many :enrollments
 
-  delegate :title, to: :coding_class
+  def student_name_list
+    names_list = []
+    self.enrollments.each do |enrollment|
+      names_list << "#{enrollment.student.first_name} #{enrollment.student.last_name}"
+    end  
+    names_list
+  end
+
+  def student_email_list
+    self.enrollments.map do |enrollment|
+      enrollment.student.email
+    end
+  end
 end
